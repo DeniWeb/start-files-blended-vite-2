88a0979602424c86a6f723d6038684a19c48e9fd
denis_stepanok <elfdenis@gmail.com>
2025-04-06 09:32:40 +0300

    todos

--- src/components/TodoListItem/TodoListItem.jsx
+++ src/components/TodoListItem/TodoListItem.jsx
@@ -1,6 +1,28 @@
-//import { RiDeleteBinLine, RiEdit2Line } from 'react-icons/ri';
-const TodoListItem = () => {
-  return <h3>TodoListItem</h3>;
+import { RiDeleteBinLine, RiEdit2Line } from 'react-icons/ri';
+import style from './TodoListItem.module.css';
+import Text from '../Text/Text';
+
+const TodoListItem = ({ text, number, id, onDelete }) => {
+  return (
+    <div className={style.box}>
+      <Text textAlign="center" marginBottom="20">
+        TODO #{number + 1}
+      </Text>
+      <Text>{text}</Text>
+
+      <button
+        onClick={() => onDelete(id)}
+        className={style.deleteButton}
+        type="button"
+      >
+        <RiDeleteBinLine size={24} />
+      </button>
+
+      <button className={style.editButton} type="button">
+        <RiEdit2Line size={24} />
+      </button>
+    </div>
+  );
 };
 
 export default TodoListItem;
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,9 +1,79 @@
+import { useEffect, useState } from 'react';
+import { nanoid } from 'nanoid';
+import Form from '../components/Form/Form';
 import Text from '../components/Text/Text';
+import TodoList from '../components/TodoList/TodoList';
+import EditForm from '../components/EditForm/EditForm';
 
 const Todos = () => {
+  const [todos, setTodos] = useState(() => {
+    const savedTodos = localStorage.getItem('todos');
+
+    return savedTodos
+      ? JSON.parse(savedTodos)
+      : [
+          { id: '1', text: 'Practice more' },
+          { id: '2', text: 'Get all tasks done on time' },
+        ];
+  });
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [currentTodo, setCurrentTodo] = useState({});
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const addNewTodo = inputValue => {
+    setTodos(prev => [
+      ...prev,
+      {
+        id: nanoid(),
+        text: inputValue,
+      },
+    ]);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const input = form.elements.search;
+    const inputValue = input.value.trim();
+
+    if (!inputValue) return;
+
+    addNewTodo(inputValue);
+    form.reset();
+  };
+
+  const handleDeleteToDo = id => {
+    setTodos(prevToDo => prevToDo.filter(todo => todo.id !== id));
+  };
+
+  const handleEditTodo = () => {};
+
+  const cancelUpdate = () => {};
+
+  const defaultValue = () => {};
+
   return (
     <>
-      <Text textAlign="center">There are no any todos ...</Text>
+      {isEditing ? (
+        <EditForm
+          updateTodo={handleEditTodo}
+          cancelUpdate={cancelUpdate}
+          defaultValue={defaultValue}
+        />
+      ) : (
+        <Form onSubmit={handleSubmit} />
+      )}
+
+      {todos.length > 0 ? (
+        <TodoList onDelete={handleDeleteToDo} todos={todos} />
+      ) : (
+        <Text textAlign="center">There are no any todos ...</Text>
+      )}
     </>
   );
 };
